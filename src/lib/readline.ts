7cfd2792e673220957f9f4c10596291a08785fd3
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-02-13 19:46:07 +1100

    refactor: Skip confirmation

--- src/lib/readline.ts
+++ src/lib/readline.ts
@@ -1,14 +1,18 @@
 import { stdin, stdout } from "process"
 import { createInterface } from "readline"
 
-export async function askConfirmation(yesCallback: () => {}) {
-    const rl = createInterface(stdin, stdout)
+export function askConfirmation(skip: boolean = false, callback: () => {}): Promise<void> {
+  if (skip) {
+    callback()
+    return
+  }
+  const rl = createInterface(stdin, stdout)
 
-    rl.question("\n💬 Confirm? yes[y]/no[n]: ", async function (answer) {
-        if (answer.match(/[Yy](es)?/g)) {
-            await yesCallback()
-        }
+  rl.question("\n💬 Confirm? yes[y]/no[n]: ", function (answer) {
+    if (answer.match(/[Yy](es)?/g)) {
+      callback()
+    }
 
-        rl.close()
-    })
+    rl.close()
+  })
 }
